5b33743b294c059503956a61bf683bae8b6066ae
yyuulan <jy2725@columbia.edu>
2018-12-08 17:32:55 -0800

    12/08 first push

--- http_caller.py
+++ http_caller.py
@@ -6,3 +6,9 @@ class HttpCaller:
     def get(self, url):
         response = urllib.request.urlopen(url)
         return json.load(response)
+
+    def get_historical_quotes_url(self, stock, interval, span, bounds):
+        url = 'https://api.robinhood.com/quotes/historicals/' + stock.upper() + \
+              '/?&interval=' + interval.lower() + '&span=' + span.lower() + \
+              '&bounds=' + bounds.lower()
+        return url
